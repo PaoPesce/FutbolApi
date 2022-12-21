4419a614cdd11df9e6090164acd3c2ba04b42434
paola pesce <paojpesce@gmail.com>
2022-12-21 02:23:41 -0300

    mejora del seed con images

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Player.create(
   team: 'Aston Villa',
   number: '23',
   position: 'Arquero',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/00/f9/00f97e8b0914b5eb213f1d7931ad6cda2bc51ec9.jpeg'
 )
 
 Player.create(
@@ -22,7 +22,7 @@ Player.create(
   team: 'Tottenham Hotspur',
   number: '13',
   position: 'Defensa central',
-  images_url: ''
+  images_url: 'https://media.tycsports.com/files/2022/09/22/482409/cristian-romero_w416.webp'
 )
 
 Player.create(
@@ -38,7 +38,7 @@ Player.create(
   team: 'Real Betis Balompie',
   number: '6',
   position: 'Defensa central',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/a1/c4/a1c4c99c808b11c4c9845601a405de9686508f01.jpeg'
 )
 
 Player.create(
@@ -46,7 +46,7 @@ Player.create(
   team: 'SL Benfica',
   number: '19',
   position: 'Defensa central',
-  images_url: ''
+  images_url: 'https://e00-ar-marca.uecdn.es/claro/assets/multimedia/imagenes/2021/08/27/16300181979492.jpg'
 )
 
 Player.create(
@@ -62,7 +62,7 @@ Player.create(
   team: 'Olympique de Lyon',
   number: '3',
   position: 'Lateral Izquierdo',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/a5/71/a5719cc2710319560bfc510cacd53315402fee82.jpeg'
 )
 
 Player.create(
@@ -70,7 +70,7 @@ Player.create(
   team: 'Villarreal CF',
   number: '2',
   position: 'Lateral derecho',
-  images_url: ''
+  images_url: 'https://deportebox.com/wp-content/uploads/2019/06/64862566_2392358434147901_2486911219731005440_n.jpg'
 )
 
 Player.create(
@@ -78,7 +78,7 @@ Player.create(
   team: 'Atletico de Madrid',
   number: '26',
   position: 'Lateral derecho',
-  images_url: ''
+  images_url: 'https://noticiasargentinas.com/images/NA/Contenidos/Molina.jpg'
 )
 
 Player.create(
@@ -86,7 +86,7 @@ Player.create(
   team: 'Sevilla FC',
   number: '4',
   position: 'Lateral derecho',
-  images_url: ''
+  images_url: 'https://bolavip.com/export/sites/lpm/img/2021/06/14/montiel_1.jpg_88783060.jpg'
 )
 
 Player.create(
@@ -94,7 +94,7 @@ Player.create(
   team: 'Real Betis Balompie',
   number: '18',
   position: 'Pivote',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/08/69/0869d8fcfe7001532f5191d7a526f449af6d406b.jpeg'
 )
 
 Player.create(
@@ -102,7 +102,7 @@ Player.create(
   team: 'Juventus de Turin',
   number: '5',
   position: 'Pivote',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/9a/af/9aaf5ff6151aae3ba9c858b0855896ee51029cdc.jpeg'
 )
 
 Player.create(
@@ -110,7 +110,7 @@ Player.create(
   team: 'Atletico de Madrid',
   number: '7',
   position: 'Mediocentro',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/84/9c/849cd0dac238f8dcf326a8ff0338776d52f969fb.jpeg'
 )
 
 Player.create(
@@ -118,7 +118,7 @@ Player.create(
   team: 'SL Benfica',
   number: '24',
   position: 'Mediocentro',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/39/5e/395e0a356d37701413a4d32a7a4dccab7b61e97e.jpeg'
 )
 
 Player.create(
@@ -126,7 +126,7 @@ Player.create(
   team: 'Brighton & Hove Albion',
   number: '20',
   position: 'Mediocentro',
-  images_url: ''
+  images_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Alexis_Mac_Allister_WC_2022.jpg/640px-Alexis_Mac_Allister_WC_2022.jpg'
 )
 
 Player.create(
@@ -134,7 +134,7 @@ Player.create(
   team: 'Bayer Leverkusen',
   number: '14',
   position: 'Mediocentro',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/85/fc/85fcf8d0773348c60f1f452276bfa3bc076698b0.jpeg'
 )
 
 Player.create(
@@ -142,7 +142,7 @@ Player.create(
   team: 'Atlanta United FC',
   number: '16',
   position: 'Mediocentro Ofensivo',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/23/9c/239c1196af5b83878768c7cb7855888304185f27.jpeg'
 )
 
 Player.create(
@@ -150,7 +150,7 @@ Player.create(
   team: 'Sevilla FC',
   number: '17',
   position: 'Extremo izquierdo',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/06/58/0658e272840dbec149f146b3d98ff2b15a010cab.jfif'
 )
 
 Player.create(
@@ -158,7 +158,7 @@ Player.create(
   team: 'Paris Saint-Germain',
   number: '10',
   position: 'Extremo derecho',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/4c/9c/4c9c6dfeb4a4baf52458256fc18c1693d6b5df4e.jpeg'
 )
 
 Player.create(
@@ -166,7 +166,7 @@ Player.create(
   team: 'Atletico de Madrid',
   number: '15',
   position: 'Extremo derecho',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/94/98/9498fa9f6c8b624c88acdf7c474aee1ae9fcab17.jpeg'
 )
 
 Player.create(
@@ -174,7 +174,7 @@ Player.create(
   team: 'Juventus de Turin',
   number: '11',
   position: 'Extremo derecho',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/c2/3a/c23a16479cfad649d4f1c46aedceb663d289e6dc.jpeg'
 )
 
 Player.create(
@@ -182,7 +182,7 @@ Player.create(
   team: 'AS Roma',
   number: '21',
   position: 'Mediapunta',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/c8/44/c844ef512a0598b6a740c33f7c411c76877d74be.jpeg'
 )
 
 Player.create(
@@ -190,7 +190,7 @@ Player.create(
   team: 'Inter de Milan',
   number: '22',
   position: 'Delantero centro',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/6b/e6/6be6581e9fcd4412ceff337c0d499302e0c1b60f.jpeg'
 )
 
 Player.create(
@@ -198,7 +198,7 @@ Player.create(
   team: 'Manchester City',
   number: '9',
   position: 'Delantero centro',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/06/f0/06f06bb4c8b4525c501672cc99969628b7e3b56a.jpeg'
 )
 
 Player.create(
@@ -206,7 +206,7 @@ Player.create(
   team: 'Villarreal CF',
   number: '12',
   position: 'Arquero',
-  images_url: ''
+  images_url: 'https://s.hs-data.com/bilder/spieler/gross/198563.jpg'
 )
 
 Player.create(
@@ -214,7 +214,7 @@ Player.create(
   team: 'CA River Plate',
   number: '1',
   position: 'Arquero',
-  images_url: ''
+  images_url: 'https://www.elgrafico.com.ar/media/cache/pub_news_details_large/media/i/5e/1d/5e1dd3fb7a37e8e528a6e4b32a8d51d1fc767770.jpeg'
 )
 
 Player.create(
@@ -263,7 +263,8 @@ Player.create(
   name: 'Lucas Hernandez',
   team: 'Bayern Munich',
   number: '21',
-  position: 'Defensa central'
+  position: 'Defensa central',
+  images_url: 'https://www.ole.com.ar/2021/10/18/mb7ygB-FV_340x340__1.jpg'
 )
 
 Player.create(
@@ -291,7 +292,8 @@ Player.create(
   name: 'Theo Hernandez',
   team: 'AC Milan',
   number: '22',
-  position: 'Lateral izquierdo'
+  position: 'Lateral izquierdo',
+  images_url: 'https://img.asmedia.epimg.net/resizer/M7V-w01dLoUy7Yzgo2x1vSciWNM=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/NTOU7GAMDVPFTHYFI66FUGHZ6E.jpg'
 )
 
 Player.create(
@@ -361,14 +363,16 @@ Player.create(
   name: 'Antoine Griezmann',
   team: 'Atletico de Madrid',
   number: '7',
-  position: 'Mediapunta'
+  position: 'Mediapunta',
+  images_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Antoine_Griezmann_in_2018.jpg/150px-Antoine_Griezmann_in_2018.jpg'
 )
 
 Player.create(
   name: 'Kylian Mbappe',
   team: 'Paris Saint-Germanin FC',
   number: '10',
-  position: 'Delantero centro'
+  position: 'Delantero centro',
+  images_url: 'https://www.ole.com.ar/2022/09/19/LfkBre8F1_720x0__1.jpg'
 )
 
 Player.create(
